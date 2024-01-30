2f0989371addcf8637a85ff4f4736cb62a1d5411
carmine <carmine.coppola001@studenti.uniparthenope.it>
2024-01-31 00:00:44 +0100

    Padding

--- exercises/fristTest/main.cpp
+++ exercises/fristTest/main.cpp
@@ -1,4 +1,4 @@
-//! [includes]
+//OpenCV includes
 #include <opencv4/opencv2/opencv.hpp>
 #include <opencv4/opencv2/imgproc/imgproc.hpp>
 #include <opencv4/opencv2/highgui/highgui.hpp>
@@ -6,34 +6,31 @@
 #include <iostream>
 
 using namespace cv;
-//! [includes]
+using namespace std;
 
 int main()
 {
-    //! [imread]
-    std::string image_path = samples::findFile("img/starry_night.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    //! [imread]
+    //Effettuo la lettura dell'immagine
+    string image_path = samples::findFile("img/starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);// imread()
+    
 
     //! [empty]
-    if(img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
+    if(img.empty()){
+        cout << "Could not read the image: " << image_path << endl;
         return 1;
     }
-    //! [empty]
 
-    //! [imshow]
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
-    //! [imshow]
+    
+    imshow("Display window", img); //Mostra l'immagine nella finestra
+    int k = waitKey(0); // Attende che premiamo un tasto sull'immagine
+    
 
-    //! [imsave]
-    if(k == 's')
-    {
-        imwrite("starry_night.png", img);
+    
+    if(k == 's'){
+        imwrite("starry_night.png", img); //Salva l'immagine
     }
-    //! [imsave]
+    
 
     return 0;
 }
